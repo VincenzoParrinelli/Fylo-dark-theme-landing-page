ba3b16b9bee7437d4347824be011489a5c0dc277
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-04-22 19:35:42 +0200

    first commit

---
+++ src/Components/LandingPage.tsx
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+  return (
+    <div className="landing-page">
+        <header className="landing-page__header">
+            
+        </header>
+    </div>
+  )
+}
