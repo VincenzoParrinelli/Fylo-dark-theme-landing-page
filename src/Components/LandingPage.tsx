39ffcfd5af10fa70fd3dbf62811c9e9c2c01dee0
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-04-22 21:15:30 +0200

    fix(scss): font-face not loading fonts

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,9 +1,17 @@
+import { ReactComponent as Logo } from "../assets/Images/logo.svg"
+
 export default function LandingPage() {
-  return (
-    <div className="landing-page">
-        <header className="landing-page__header">
-            
-        </header>
-    </div>
-  )
+    return (
+        <div className="landing-page">
+            <header className="landing-page__header">
+                <Logo className="landing-page__logo"/>
+
+                <nav className="landing-page__navbar">
+                    <a href="#" className="landing-page__link">Features</a>
+                    <a href="#" className="landing-page__link">Team</a>
+                    <a href="#" className="landing-page__link">Sign In</a>
+                </nav>
+            </header>
+        </div>
+    )
 }
