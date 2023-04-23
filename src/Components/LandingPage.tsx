25a456af7026ef6f5d577d0c51f0b9c4ddb49034
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-04-23 02:06:11 +0200

    feat(LandingPage): added header

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,17 +1,36 @@
-import { ReactComponent as Logo } from "../assets/Images/logo.svg"
+import { ReactComponent as Logo } from "../../public/Assets/Images/logo.svg"
+import { ReactComponent as Headerbackground } from "../../public/assets/Images/bg-curvy-desktop.svg"
 
 export default function LandingPage() {
     return (
         <div className="landing-page">
+
             <header className="landing-page__header">
-                <Logo className="landing-page__logo"/>
 
                 <nav className="landing-page__navbar">
-                    <a href="#" className="landing-page__link">Features</a>
-                    <a href="#" className="landing-page__link">Team</a>
-                    <a href="#" className="landing-page__link">Sign In</a>
+                    <Logo className="landing-page__logo" />
+
+                    <div className="landing-page__nav-items">
+                        <a href="#" className="landing-page__link">Features</a>
+                        <a href="#" className="landing-page__link">Team</a>
+                        <a href="#" className="landing-page__link">Sign In</a>
+                    </div>
                 </nav>
+
+
+                <div className="landing-page__test">
+                    <img src="../../public/Assets/Images/illustration-intro.png" alt="illustration" className="landing-page__illustration-intro" />
+
+                    <h1 className="heading-primary">All your files in one secure location, accessible anywhere.</h1>
+                </div>
+
+
             </header>
+
+
+            <main>
+
+            </main>
         </div>
     )
 }
