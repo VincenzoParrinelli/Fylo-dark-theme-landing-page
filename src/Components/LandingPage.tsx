8493a44e8706a2998146957dc9940ce955144b31
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-05-25 00:55:47 +0200

    refact(stayProductive): broke down into his own component

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,8 +1,8 @@
 import Header from "./Header"
 import Features from "./Features"
+import StayProductive from "./StayProductive"
 
 import { ReactComponent as Logo } from "../../Assets/Images/logo.svg"
-import { ReactComponent as ArrowIcon } from "../../assets/Images/icon-arrow.svg"
 import { ReactComponent as PositionIcon } from "../../assets/Images/position-marker-svgrepo-com.svg"
 import { ReactComponent as PhoneRingingIcon } from "../../assets/Images/phone-call-ringing-svgrepo-com.svg"
 import { ReactComponent as GmailIcon } from "../../assets/Images/gmail-svgrepo-com.svg"
@@ -20,37 +20,7 @@ export default function LandingPage() {
 
                 <Features />
 
-                <section className="landing-page__stay-productive-section">
-                    <img
-                        className="landing-page__stay-productive-illustration"
-                        src="../../assets/Images/illustration-stay-productive.png" alt="stay-productive-illustration"
-                    />
-
-                    <div className="landing-page__stay-productive-container">
-
-                        <div className="landing-page__stay-productive-heading-container">
-                            <h1 className="heading-primary heading-primary--left u-clear-line-height">Stay productive, wherever you are</h1>
-                        </div>
-
-                        <p className="paragraph-secondary u-margin-bottom-small u-text-left">
-                            Never let location be an issue when accessing your files. Fylo has you covered for all of your life storage needs.
-                        </p>
-
-                        <p className="paragraph-secondary u-text-left">
-                            Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments
-                            required.
-                        </p>
-
-                        <a href="#" className="paragraph-tertiary paragraph-tertiary--anchor">
-
-                            See how Fylo works
-
-                            <ArrowIcon className="landing-page__arrow-icon" />
-                        </a>
-                    </div>
-
-                </section>
-
+                <StayProductive />
 
                 <section className="landing-page__cards-section">
                     <div className="landing-page__card">
---
+++ src/Components/StayProductive.tsx
@@ -0,0 +1,36 @@
+import { ReactComponent as ArrowIcon } from "../../assets/Images/icon-arrow.svg"
+
+export default function StayProductive() {
+    return (
+        <section className="stay-productive">
+            <img
+                className="stay-productive__illustration"
+                src="../../assets/Images/illustration-stay-productive.png" alt="stay-productive-illustration"
+            />
+
+            <div className="stay-productive__container">
+
+                <div className="stay-productive__heading-container">
+                    <h1 className="heading-primary heading-primary--left u-clear-line-height">Stay productive, wherever you are</h1>
+                </div>
+
+                <p className="paragraph-secondary u-margin-bottom-small u-text-left">
+                    Never let location be an issue when accessing your files. Fylo has you covered for all of your life storage needs.
+                </p>
+
+                <p className="paragraph-secondary u-text-left">
+                    Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments
+                    required.
+                </p>
+
+                <a href="#" className="paragraph-tertiary paragraph-tertiary--anchor">
+
+                    See how Fylo works
+
+                    <ArrowIcon className="arrow-icon" />
+                </a>
+            </div>
+
+        </section>
+    )
+}
