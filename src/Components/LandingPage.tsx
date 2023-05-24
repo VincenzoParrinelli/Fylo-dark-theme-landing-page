7e993587227235c778bb4dc1c84a283cb147a76b
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-05-24 20:29:08 +0200

    feat(landingPage): early access form

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -192,6 +192,27 @@ export default function LandingPage() {
                 </section>
             </main>
 
+            <div className="landing-page__early-access-form">
+
+                <div className="landing-page__early-access-form-container">
+
+                    <h1 className="heading-primary heading-primary--medium">Get early access today</h1>
+
+                    <p className="paragraph-secondary u-inline-size-large">
+                        It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions,
+                        our support team would be happy to help you.
+                    </p>
+
+                    <div className="landing-page__email-container">
+                        <input type="email" className="landing-page__email-input" placeholder="email@example.com" />
+
+                        <button className="btn btn--secondary">Get Started For Free</button>
+                    </div>
+
+                </div>
+
+            </div>
+
             <footer className="landing-page__footer">
 
                 <Logo className="landing-page__footer-logo" />
