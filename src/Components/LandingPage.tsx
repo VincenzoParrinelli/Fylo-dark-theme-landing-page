45beb576dde2c3ef54b535223122008d26523d2d
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-05-24 18:50:56 +0200

    feat(landingPage): added footer

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -4,6 +4,12 @@ import { ReactComponent as SecurityIcon } from "../../assets/Images/icon-securit
 import { ReactComponent as CollaborationIcon } from "../../assets/Images/icon-collaboration.svg"
 import { ReactComponent as AnyFileIcon } from "../../assets/Images/icon-any-file.svg"
 import { ReactComponent as ArrowIcon } from "../../assets/Images/icon-arrow.svg"
+import { ReactComponent as PositionIcon } from "../../assets/Images/position-marker-svgrepo-com.svg"
+import { ReactComponent as PhoneRingingIcon } from "../../assets/Images/phone-call-ringing-svgrepo-com.svg"
+import { ReactComponent as GmailIcon } from "../../assets/Images/gmail-svgrepo-com.svg"
+import { ReactComponent as FacebookIcon } from "../../assets/Images/facebook-176-svgrepo-com.svg"
+import { ReactComponent as Twittericon } from "../../assets/Images/twitter-154-svgrepo-com.svg"
+import { ReactComponent as Instagramicon } from "../../assets/Images/instagram-167-svgrepo-com.svg"
 
 export default function LandingPage() {
     return (
@@ -185,6 +191,77 @@ export default function LandingPage() {
                     </div>
                 </section>
             </main>
+
+            <footer className="landing-page__footer">
+
+                <Logo className="landing-page__footer-logo" />
+
+                <ul className="landing-page__footer-info-navbar">
+
+                    <li className="landing-page__footer-info">
+
+                        <PositionIcon className="landing-page__footer-icon" />
+
+                        <p className="paragraph-secondary u-text-left u-inline-size-medium">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore fugiat nostrum qui veniam delectus? Modi beatae sed asperiores?
+                        </p>
+
+                    </li>
+
+                    <li className="landing-page__footer-info">
+
+                        <PhoneRingingIcon className="landing-page__footer-icon" />
+
+                        <p className="paragraph-secondary u-text-left">
+                            +1-543-123-4567
+                        </p>
+
+                    </li>
+
+                    <li className="landing-page__footer-info">
+
+                        <GmailIcon className="landing-page__footer-icon" />
+
+                        <p className="paragraph-secondary u-text-left">
+                            example@fylo.com
+                        </p>
+
+                    </li>
+
+                    <li className="landing-page__footer-info">
+
+                        <a href="#" className="paragraph-secondary u-padding-bottom-small u-text-left">About us</a>
+                        <a href="#" className="paragraph-secondary u-padding-bottom-small u-text-left">Jobs</a>
+                        <a href="#" className="paragraph-secondary u-padding-bottom-small u-text-left">Press</a>
+                        <a href="#" className="paragraph-secondary u-padding-bottom-small u-text-left">Blog</a>
+
+                    </li>
+
+                    <li className="landing-page__footer-info">
+
+                        <a href="#" className="paragraph-secondary u-padding-bottom-small u-text-left">Contact us</a>
+                        <a href="#" className="paragraph-secondary u-padding-bottom-small u-text-left">Terms</a>
+                        <a href="#" className="paragraph-secondary u-padding-bottom-small u-text-left">Privacy</a>
+
+                    </li>
+
+                    <li className="landing-page__footer-info">
+
+                        <div className="landing-page__footer-social-icon-container">
+                            <FacebookIcon className="landing-page__footer-social-icon" />
+                        </div>
+
+                        <div className="landing-page__footer-social-icon-container">
+                            <Twittericon className="landing-page__footer-social-icon" />
+                        </div>
+
+                        <div className="landing-page__footer-social-icon-container">
+                            <Instagramicon className="landing-page__footer-social-icon" />
+                        </div>
+
+                    </li>
+                </ul>
+            </footer>
         </div>
     )
 }
