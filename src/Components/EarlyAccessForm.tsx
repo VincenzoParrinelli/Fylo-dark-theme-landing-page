448762432631b7d20057542efa7ea13e190f10d0
VincenzoParrinelli <vincenzoparrinelli01@gmail.com>
2023-05-27 00:31:51 +0200

    feat(EarlyAccessForm): error message for email input

--- src/Components/EarlyAccessForm.tsx
+++ src/Components/EarlyAccessForm.tsx
@@ -1,4 +1,29 @@
+import { useState, useRef, useEffect } from "react"
+
 export default function EarlyAccessForm() {
+
+    const [isInvalid, setIsInvalid] = useState(false)
+
+    const emailInputRef = useRef() as React.MutableRefObject<HTMLInputElement>
+    const errorMessageRef = useRef() as React.MutableRefObject<HTMLParagraphElement>
+
+    // Upon isInvalid changes, set opacity respectively, in order to get the error element to appear 
+    useEffect(() => {
+
+        if (isInvalid) errorMessageRef.current.style.opacity = "1"
+        else errorMessageRef.current.style.opacity = "0"
+
+    }, [isInvalid])
+
+    const handleForm = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+
+        // Check if emailInput is empty or has a type mismatch.
+        // If one of these 2 conditions is true set isInvalid flag to true else set it to false
+        if (!emailInputRef.current.value || emailInputRef.current.validity.typeMismatch) setIsInvalid(true)
+        else setIsInvalid(false)
+    }
+
     return (
         <div className="early-access-form">
 
@@ -12,12 +37,24 @@ export default function EarlyAccessForm() {
                 </p>
 
                 <div className="early-access-form__email-container">
-                    <input type="email" className="early-access-form__email-input" placeholder="email@example.com" />
+                    <input
+                        ref={emailInputRef}
+                        type="email"
+                        className="early-access-form__email-input"
+                        placeholder="email@example.com"
+                    />
 
-                    <button className="btn btn--secondary">Get Started For Free</button>
+                    <button
+                        className="btn btn--secondary"
+                        onClick={e => handleForm(e)}
+                    >
+                        Get Started For Free
+                    </button>
                 </div>
 
             </div>
+
+            <p ref={errorMessageRef} className="paragraph-tertiary paragraph-tertiary--error-paragraph">Please enter a valid email address</p>
 
         </div>
     )
